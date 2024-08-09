9286b1b33e5d852784187537ae507ae45bf3b8e2
tomas-purplefire <tomas@purplefire.io>
2024-08-09 03:53:34 -0300

    need better quality img

---
+++ src/assets/svgs/TickSvg.tsx
@@ -0,0 +1,24 @@
+export function Tick() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <g clipPath="url(#clip0_221_144)">
+        <path
+          stroke="#fff"
+          strokeWidth="1.6"
+          d="M6.4 12l4.8 4 6.4-8M12 23.2A11.2 11.2 0 1112 .798 11.2 11.2 0 0112 23.2z"
+        ></path>
+      </g>
+      <defs>
+        <clipPath id="clip0_221_144">
+          <path fill="#fff" d="M0 0H24V24H0z"></path>
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
--- src/slices/ContentWithImage/index.tsx
+++ src/slices/ContentWithImage/index.tsx
@@ -1,25 +1,22 @@
+import React from "react";
 import { Content } from "@prismicio/client";
 import { PrismicNextImage } from "@prismicio/next";
 import { SliceComponentProps } from "@prismicio/react";
 import { CTA } from "@/components/CTA";
 import Image from "next/image";
+import { Tick } from "@/assets/svgs/TickSvg";
 
-/**
- * Props for `ContentWithImage`.
- */
 export type ContentWithImageProps =
   SliceComponentProps<Content.ContentWithImageSlice>;
 
-/**
- * Component for "ContentWithImage" Slices.
- */
 const ContentWithImage = ({ slice }: ContentWithImageProps): JSX.Element => {
   const isImgRight = slice.variation === "imageRight";
+
   return (
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
-      className="content-with-image-layout"
+      className={`content-with-image-layout ${isImgRight ? "imgrightvar" : ""}`}
     >
       <div
         className={`content-with-image-text ${isImgRight ? "imgrightvar" : ""}`}
@@ -29,33 +26,39 @@ const ContentWithImage = ({ slice }: ContentWithImageProps): JSX.Element => {
         <p>{slice.primary.description}</p>
         {isImgRight && (
           <>
-            {slice.primary.tick_field.map((item) => {
-              <div
-                className="content-with-image-tick-fields"
-                key={item.tick_description}
-              >
-                <span>{item.tick_icon}</span>
+            {slice.primary.tick_field.map((item, index) => (
+              <div className="content-with-image-tick-field" key={index}>
+                <span>
+                  <Tick />
+                </span>
                 <p>{item.tick_description}</p>
-              </div>;
-            })}
+              </div>
+            ))}
           </>
         )}
-        <CTA variant="primary" field={slice.primary.button_link}>
+        <CTA
+          variant={isImgRight ? "secondary" : "primary"}
+          field={slice.primary.button_link}
+        >
           {slice.primary.button_text}
         </CTA>
       </div>
-      <div className="content-with-image-image">
+      <div
+        className={`content-with-image-image ${isImgRight ? "imgrightvar" : ""}`}
+      >
         <PrismicNextImage
           field={slice.primary.image}
           className="content-with-image-main-img"
         />
-        <Image
-          className="deco-img-over-img"
-          src="https://ik.imagekit.io/2ziqnactl/Luvianka/deco-image.png"
-          alt="jam image - decoration purpourses"
-          width={512}
-          height={512}
-        />
+        {!isImgRight && (
+          <Image
+            className="deco-img-over-img"
+            src="https://ik.imagekit.io/2ziqnactl/Luvianka/deco-image.png"
+            alt="jam image - decoration purposes"
+            width={512}
+            height={512}
+          />
+        )}
       </div>
     </section>
   );
