e49836bbd905e4811b38b821c792bcfcd8c14fd9
tomas-purplefire <tomas@purplefire.io>
2024-08-25 05:17:39 -0300

    refactor:contentWithImg slice refactored component ans SCSS

--- src/slices/ContentWithImage/index.tsx
+++ src/slices/ContentWithImage/index.tsx
@@ -3,6 +3,7 @@ import { Content } from "@prismicio/client";
 import { PrismicNextImage } from "@prismicio/next";
 import { SliceComponentProps } from "@prismicio/react";
 import { CTA } from "@/components/CTA";
+import { Container } from "@/components/Container";
 import Image from "next/image";
 import { Tick } from "@/assets/svgs/TickSvg";
 import classNames from "classnames";
@@ -11,65 +12,75 @@ export type ContentWithImageProps =
   SliceComponentProps<Content.ContentWithImageSlice>;
 
 const ContentWithImage = ({ slice }: ContentWithImageProps): JSX.Element => {
-  const isImgRight = slice.variation === "imageRight";
+  const isVar = slice.variation === "imageRight";
 
   return (
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
-      className={`content-with-image ${isImgRight ? "imgrightvar" : ""}`}
+      className={classNames("content-with-image", {
+        "content-with-image--var": isVar,
+      })}
     >
-      <div
-        className={`content-with-image-layout ${isImgRight ? "imgrightvar" : ""}`}
-      >
+      <Container className="container-lg">
         <div
-          className={`content-with-image-text ${isImgRight ? "imgrightvar" : ""}`}
-        >
-          <h1>{slice.primary.heading}</h1>
-          <h2>{slice.primary.sub_heading}</h2>
-          <p>{slice.primary.description}</p>
-          {isImgRight && (
-            <>
-              {slice.primary.tick_field.map((item, index) => (
-                <div className="content-with-image-tick-field" key={index}>
-                  <span>
-                    <Tick />
-                  </span>
-                  <p>{item.tick_description}</p>
-                </div>
-              ))}
-            </>
-          )}
-          <CTA
-            variant={isImgRight ? "secondary" : "primary"}
-            link={slice.primary.button_link}
-            label={slice.primary.button_text}
-          />
-        </div>
-        <div
-          className={`content-with-image-image ${isImgRight ? "imgrightvar" : ""}`}
+          className={classNames("content-with-image__layout", {
+            "content-with-image__layout--var": isVar,
+          })}
         >
           <div
-            className={classNames("main-image-wrapper", {
-              "main-image-wrapper-imgright": isImgRight,
+            className={classNames("content-with-image__text", {
+              "content-with-image__text--var": isVar,
             })}
           >
-            <PrismicNextImage
-              field={slice.primary.image}
-              className="content-with-image-main-img"
-            />
-            {!isImgRight && (
-              <Image
-                className="deco-img-over-img"
-                src="https://ik.imagekit.io/2ziqnactl/Luvianka/deco-image.png"
-                alt="jam image - decoration purposes"
-                width={512}
-                height={512}
-              />
+            <h1>{slice.primary.heading}</h1>
+            <h2>{slice.primary.sub_heading}</h2>
+            <p>{slice.primary.description}</p>
+            {isVar && (
+              <>
+                {slice.primary.tick_field.map((item, index) => (
+                  <div className="content-with-image__tick-field" key={index}>
+                    <span>
+                      <Tick />
+                    </span>
+                    <p>{item.tick_description}</p>
+                  </div>
+                ))}
+              </>
             )}
+            <CTA
+              variant={isVar ? "secondary" : "primary"}
+              link={slice.primary.button_link}
+              label={slice.primary.button_text}
+            />
+          </div>
+          <div
+            className={classNames("content-with-image__bg-image-container", {
+              "content-with-image__bg-image-container--var": isVar,
+            })}
+          >
+            <div
+              className={classNames("col-image", {
+                "col-image--var": isVar,
+              })}
+            >
+              <PrismicNextImage
+                field={slice.primary.image}
+                className="col-image_img"
+              />
+              {!isVar && (
+                <Image
+                  className="deco-img"
+                  src="https://ik.imagekit.io/2ziqnactl/Luvianka/deco-image.png"
+                  alt="jam image - decoration purposes"
+                  width={512}
+                  height={512}
+                />
+              )}
+            </div>
           </div>
         </div>
-      </div>
+      </Container>
     </section>
   );
 };
