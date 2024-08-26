7a597f3e171ce323660e0012519eb8189d8b0f53
tomas-purplefire <tomas@purplefire.io>
2024-08-26 08:12:58 -0300

    Footer component and scss refactored

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { createClient } from "@/prismicio";
 import Link from "next/link";
 import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
+
+import { Section } from "@/components/Section";
+
 import { Facebook } from "@/assets/svgs/Facebook";
 import { Instagram } from "@/assets/svgs/Instagram";
 import { Twitter } from "@/assets/svgs/Twitter";
@@ -21,66 +24,102 @@ export const Footer = async () => {
     return null;
   }
 
+  const brandingPieces = homepage.data.corporation_section;
+  const contactPoints = homepage.data.contact_data;
+
   return (
-    <footer className="footer">
-      <div className="footer-content">
-        {homepage?.data.corporation_section.map((item) => {
-          return (
-            <div key={item.brief_text} className="footer-corporate-section">
-              <div className="corporate-section-images">
+    <Section>
+      <footer className="footer">
+        <section className="footer__content">
+          {brandingPieces.map(({ brief_text, first_logo, second_logo }) => (
+            <div
+              aria-label="Logos de Luvianka y Red Deer"
+              key={brief_text}
+              className="branding-space"
+            >
+              <div className="branding-space__images">
                 <Link href="/">
-                  <PrismicNextImage field={item.first_logo} />
+                  <PrismicNextImage field={first_logo} />
                 </Link>
-                <PrismicNextImage field={item.second_logo} />
+                <PrismicNextImage field={second_logo} />
               </div>
-              <p>{item.brief_text}</p>
+              <p>{brief_text}</p>
             </div>
-          );
-        })}
-        <nav aria-label="Footer">
-          <ul className="flow-content uppercase" role="list">
-            {settings?.data.navigation.map((item) => (
-              <li className="footer__nav-list-item" key={item.label}>
-                <PrismicNextLink field={item.link}>
-                  {item.label}
-                </PrismicNextLink>
-              </li>
-            ))}
-            <li></li>
-          </ul>
-        </nav>
-        {homepage?.data.contact_data.map((item) => (
-          <div className="footer-contact-data" key={item.fields_header}>
-            <h3>{item.fields_header}</h3>
-            <div>
-              <p>{item.field1}</p>
-            </div>
-            <p>{item.field2}</p>
-            <p>{item.field3}</p>
-            <p>{item.field4}</p>
-            <div>
-              <a
-                className="footer-email-link"
-                href="mailto:info@fiambresluvianka.com.ar"
+          ))}
+          <nav
+            className="footer__nav"
+            aria-label="Navegación del pie de página"
+          >
+            <ul className="flow-content uppercase" role="list">
+              {settings?.data.navigation.map(({ link, label }) => (
+                <li key={label} role="listitem" className="footer__nav-item">
+                  <PrismicNextLink field={link}>{label}</PrismicNextLink>
+                </li>
+              ))}
+            </ul>
+          </nav>
+          {contactPoints.map(
+            ({ field1, field2, field3, field4, field5, fields_header }) => (
+              <div
+                className="footer__contact-points flow-content"
+                key={fields_header}
               >
-                {item.field5}
-              </a>
+                <h3 className="text-accent uppercase fw-400">
+                  {fields_header}
+                </h3>
+                <div>
+                  <p>{field1}</p>
+                </div>
+                <p>{field2}</p>
+                <p>{field3}</p>
+                <p>{field4}</p>
+                <div>
+                  <a
+                    className="footer__email-link"
+                    href="mailto:info@fiambresluvianka.com.ar"
+                  >
+                    {field5}
+                  </a>
+                </div>
+              </div>
+            )
+          )}
+        </section>
+        <section
+          aria-label="Redes sociales y derechos de autor"
+          className="subfooter"
+        >
+          <div className="subfooter__content">
+            <div className="subfooter__social-media">
+              <Link
+                href="https://www.facebook.com/fiambresluvianka/"
+                aria-label="Visita nuestra página de Facebook"
+              >
+                <Facebook />
+              </Link>
+              <Link
+                href="https://www.instagram.com/fiambresluvianka/"
+                aria-label="Visita nuestro perfil de Instagram"
+              >
+                <Instagram />
+              </Link>
+              <Link
+                href="https://x.com/fluviankaok?s=11"
+                aria-label="Visita nuestro perfil de Twitter/X"
+              >
+                <Twitter />
+              </Link>
+              <Link
+                href="https://www.linkedin.com/company/fiambres-luvianka/"
+                aria-label="Visita nuestro perfil de Linkedin"
+              >
+                <Linkedin />
+              </Link>
             </div>
+            <p>© Luvianka {currentYear} Todos los derechos reservados.</p>
           </div>
-        ))}
-      </div>
-      <div className="subfooter-layout">
-        <div className="subfooter-content">
-          {/* TODO: add functionality to this icons */}
-          <div>
-            <Facebook />
-            <Instagram />
-            <Twitter />
-            <Linkedin />
-          </div>
-          <p>© Luvianka {currentYear} Todos los derechos reservados.</p>
-        </div>
-      </div>
-    </footer>
+        </section>
+      </footer>
+    </Section>
   );
 };
